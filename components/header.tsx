31ef3502b8c9c27e9ee19dc7799d9d09d3c91236
Assassin3301 <47602517+sharmaabhinay28@users.noreply.github.com>
2025-06-03 11:23:51 +0530

    header 1st completed log

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
+import logo from '@/public/logo.png'
 
 const header = () => {
   return (
-    <div className = "bg-[#1A365D] text-amber-50 justify-between flex">
-    <div className ="my-3"> 
-      <ul className = "flex my-3">
-        <li><Image src="/public/logo.png" alt ="logo" width={50} height={50}></Image></li>
-        <li className ="mx-2">Zeniotech</li>
+    <div className = "bg-[#1A365D] text-white justify-between flex">
+    <div> 
+      <ul className = "flex">
+        <li className='my-1/2'><Image src ={logo} alt ="logo" width={80} height={80}></Image></li>
+        <li className ="mx-1/2 my-7.5">Zeniotech</li>
       </ul>
     </div>
-    <ul className="flex">
-      <div className = "my-3"><li>test1</li></div>
-      <div className = "mx-5 my-3"><li>test2</li></div>
-      <div className = "my-3"><li>test3</li></div>
+    <ul className='flex justify-between'>
+      <li className='my-7.5'>Home</li>
+      <li className='mx-5 my-7.5'>Services</li>
+      <li className='my-7.5'>About Us</li>
+      <li><button className = "mx-8 border-2 border-[#767676] bg-[#FF4757] py-4 rounded-2xl w-50 my-3"> Contact Us</button></li>
     </ul>
-      <button className = "mx-10 border-2 bg-[#FF4757] rounded-2xl w-50 my-3"> Contact Us</button>
   </div>
   )
 }
