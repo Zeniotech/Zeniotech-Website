6475b5942913dbeb3b69ed0ba957ac15ef76b599
Assassin3301 <47602517+sharmaabhinay28@users.noreply.github.com>
2025-06-03 22:45:23 +0530

    changed padding of logo from x-y

--- components/header.tsx
+++ components/header.tsx
@@ -6,14 +6,16 @@ const header = () => {
   return (
     <div className = "bg-[#1A365D] text-white justify-between flex">
     <div> 
-      <ul className = "flex">
+      <ul className = "flex mx-6 my-2">
         <Link href='/'><li className='my-1/2'><Image src ={logo} alt ="logo" width={80} height={80}></Image></li></Link>
         <Link href='/'><li className='my-1/2'></li><li className ="mx-1/2 my-7.5">Zeniotech</li></Link>
       </ul>
     </div>
     <ul className='flex justify-between'>
       <Link href='/'><li className='my-7.5 hover:bg-[#7B61FF] rounded-2xl w-20 h-10 text-center py-1.5'>Home</li></Link>
+      <div>
       <Link href='/our-services'><li className='my-7.5 hover:bg-[#7B61FF] rounded-2xl w-20 h-10 text-center py-1.5'>Services</li></Link>
+      </div>
       <Link href='/about-us'><li className='my-7.5 hover:bg-[#7B61FF] rounded-2xl w-20 h-10 text-center py-1.5'>About Us</li></Link>
       <Link href='/contact-us'><li><button className = "mx-8 border-2 border-[#767676] bg-[#FF4757] py-4 rounded-2xl w-50 my-5 hover:bg-[#A41622] cursor-pointer"> Contact Us</button></li></Link>
     </ul>
