a7cbc365633c920cf9bdc53ecd287c84d166c779
Assassin3301 <47602517+sharmaabhinay28@users.noreply.github.com>
2025-06-10 00:42:22 +0530

    modified footer component according to needs

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,17 +4,44 @@ import logo from "@/public/logo.png";
 
 const footer = () => {
   return (
-<div className = 'bg-[#1A365D] h-80'>
+<div className = 'bg-[#1A365D] h-100 w-screen'>
 <div className = 'flex justify-around'>
-<div className = 'h-80 w-80'>
-<div className='flex flex-row'>
-<Image src={logo} alt="logo" width={150} height={150} className="md:mx-1/2"></Image>
-<div className='text-white'>Zeniotech</div>
+<div className = 'h-100 w-100'>
+<div className='my-20'>
+<ul className='flex'>
+<li><Image src={logo} alt="logo" width={150} height={180} className=""></Image></li>
+<li className='py-15'><div className='text-white text-3xl'>Zeniotech</div></li>
+</ul>
 </div>
-<div className='text-white'>empowering excellence through innovative solution</div>
+<div className='text-white text-2xl text-left -my-20 font-bold'>"empowering excellence through innovative solution"</div>
 </div>
-<div className = 'bg-amber-800 h-80 w-80'></div>
-<div className = 'bg-amber-600 h-80 w-80'></div>
+<div className = 'h-100 w-100'>
+  <div className='text-3xl text-white font-bold py-5'>Our Core Services</div>
+  <ul className='text-left text-xl text-white py-0.5 justify-around'>
+    <li>Servicenow</li>
+    <li>Custom Application</li>
+    <li>GRC</li>
+    <li>HRSD</li>
+    <li>SecOps</li>
+    <li>ITOM</li>
+    <li>Integrating Existing Solution</li>
+  </ul>
+</div>
+<div className = 'h-100 w-100'>
+<div className='text-3xl text-white font-bold py-5'>Contact Us</div>
+  <div>
+    <ul className='text-white text-2xl'>
+      <li>📩Sharmaabhinay28@zeniotech.com</li>
+      <li>📞+91 8847406838</li>
+      <li>📍 [New Delhi, Delhi]</li>
+      <li>Stay Updated</li>
+      <li>Abhinay@zeniotech</li>
+      <li><button className="my-5 py-10 w-100 border-2 rounded-2xl border-[#767676] bg-[#FF4757] hover:bg-[#A41622] cursor-pointer">Contact Us
+      </button>
+      </li>
+    </ul>
+  </div>
+</div>
 </div>
 </div>
   )
