d71f93b17db27ea67bec5929a9f35e6a344a6c56
Assassin3301 <47602517+sharmaabhinay28@users.noreply.github.com>
2025-07-01 23:56:57 +0530

    Added Images for itsm integration tools and created component from integration tools.

---
+++ app/itsm/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ServicenowIntegrationTools from '@/components/servicenow-integration-tools'
+
+const servicenowItsm = () => {
+  return (
+    <div className='my-10'>
+      <ServicenowIntegrationTools />
+    </div>
+  )
+}
+
+export default servicenowItsm
