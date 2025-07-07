3e4aeb753e83ca179e87ef6e8aa9f25126d3e645
Assassin3301 <47602517+sharmaabhinay28@users.noreply.github.com>
2025-07-07 22:18:43 +0530

    added component servicenow integration tools and called it in hrsd.

---
+++ components/servicenow-integration-tools.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import docuSign from '@/public/docusign.png'
+import firstAdvantage from '@/public/first_advantage.png'
+import workDay from '@/public/workday_logo.png'
+import abService from '@/public/abservice.png'
+
+const servicenowIntegrationTools = () => {
+  return (
+    <div>
+      <div className='bg-[#1A365D] h-40 flex flex-row justify-around items-center'>
+       <Image src={workDay} alt='docusign logo' className='h-30 w-30'></Image>
+       <Image src={docuSign} alt='docusign logo' className='h-30 w-30'></Image>
+       <Image src={firstAdvantage} alt='docusign logo' className='h-30 w-30'></Image>
+       <Image src={abService} alt='docusign logo' className='h-30 w-30'></Image>
+      </div>
+    </div>
+  )
+}
+
+export default servicenowIntegrationTools
