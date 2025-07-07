2ae3788666970c12d57396ccb2db90d65eaa5f9b
Assassin3301 <47602517+sharmaabhinay28@users.noreply.github.com>
2025-07-07 22:14:16 +0530

    changed hrsd component

--- app/hrsd/page.tsx
+++ app/hrsd/page.tsx
@@ -1,22 +1,18 @@
 import React from 'react'
-<<<<<<< HEAD
 import ServicenowIntegrationTools from '@/components/servicenow-integration-tools'
 import BannerHeader from '@/components/snow_page_header'
 import ContactUsBanner from '@/components/contact-us-banner'
 import Footer from '@/components/snow_page_footer'
-=======
 import Image from 'next/image'
 import docuSign from '@/public/docusign.png'
 import firstAdvantage from '@/public/first_advantage.png'
 import workDay from '@/public/workday_logo.png'
 import abService from '@/public/abservice.png'
->>>>>>> 8b8bb512fbc959b6c9bde0049db4065e987b2d7d
 
 
 const servicenowHrsd = () => {
   return (
     <div className='my-10'>
-<<<<<<< HEAD
       <BannerHeader/>
       <div className=''>
          <ServicenowIntegrationTools />
@@ -28,7 +24,6 @@ const servicenowHrsd = () => {
       </div>
 
        
-=======
        <div>
       <div className='bg-[#1A365D] h-40 flex flex-row justify-around items-center'>
        <Image src={workDay} alt='docusign logo' className='h-30 w-30'></Image>
@@ -37,7 +32,6 @@ const servicenowHrsd = () => {
        <Image src={abService} alt='docusign logo' className='h-30 w-30'></Image>
       </div>
     </div>
->>>>>>> 8b8bb512fbc959b6c9bde0049db4065e987b2d7d
     </div>
   )
 }
